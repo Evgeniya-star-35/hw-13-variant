c9e9993408cd89d7266d0eeb3d3619ff8981c4f2
Evgeniya Bayda <baje75128@gmail.com>
2021-08-28 20:08:59 +0300

    add styles

---
+++ src/services/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_KEY = "22948593-eb3728724c643cf2948c736ef";
+axios.defaults.baseURL =
+  "https://pixabay.com/api/?image_type=photo&orientation=horizontal";
+
+export async function getPictures(query, page) {
+  const {
+    data: { hits },
+  } = await axios.get(`&q=${query}&page=${page}&per_page=12&key=${API_KEY}`);
+
+  return hits;
+}
